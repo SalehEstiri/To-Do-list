70bf5cf1ede677bc6186cf3c9989e17d0258103b
Saleh Estiri <197503599+SalehEstiri@users.noreply.github.com>
2025-07-31 10:55:26 +0330

    Class task created.
    For this code it's better to use pydantic BaseModel for ensuring type validation.

--- To_do_list.py
+++ To_do_list.py
@@ -1,1 +1,8 @@
 import pandas
+import pydantic
+
+class task(pydantic.BaseModel):
+    def __init__(self, name: str, discription: str, priority: int):
+        self.name = name
+        self.discription = discription
+        self.priority = priority
