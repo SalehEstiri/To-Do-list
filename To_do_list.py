e8d055e73c80adccb01df65fa923f36ceccccc01
Saleh Estiri <197503599+SalehEstiri@users.noreply.github.com>
2025-07-31 11:27:24 +0330

    Class task modify.
    Now it's more optimize.

--- To_do_list.py
+++ To_do_list.py
@@ -2,11 +2,13 @@ import pandas
 import pydantic
 
 class task(pydantic.BaseModel):
-    def __init__(self, name: str, discription: str, priority: int):
-        self.name = name
-        self.discription = discription
-        self.priority = priority
+    name: str
+    discription: str
+    priority: int
 
 class ToDoList(task):
     def __init__(self, tasks: list[task]):
         self.tasks = tasks
+
+    def delete(self, name):
+        self.tasks.remove(name)
