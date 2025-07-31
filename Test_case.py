05a97336e09e8514b763515dc662a99eb6de034d
Saleh Estiri <197503599+SalehEstiri@users.noreply.github.com>
2025-07-31 15:35:53 +0330

    test_delete function added.

--- Test_case.py
+++ Test_case.py
@@ -8,4 +8,9 @@ class TestToDoList(unittest.TestCase):
         self.task1 = Task(name="Buy bread", description="I don't know", priority=2)
         self.task2 = Task(name="Homework", description="Do your homework", priority=1)
         self.task3 = Task(name="Exam", description="Prepare for your exam", priority=3)
-        self.todo_list = ToDoList([self.task1, self.task2, self.task3])
+        self.to_do_list = ToDoList([self.task1, self.task2, self.task3])
+
+    def test_delete(self):
+        self.to_do_list.delete("Buy bread")
+        self.assertEqual(len(self.to_do_list.tasks), 2)
+        self.assertNotIn("Buy bread", [task.name for task in self.to_do_list.tasks])
