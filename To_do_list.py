07ca7b005c52124a67999026d89f68c68f9a0e8a
Saleh Estiri <197503599+SalehEstiri@users.noreply.github.com>
2025-08-13 19:04:45 +0330

    save button connected to save function.

--- To_do_list.py
+++ To_do_list.py
@@ -33,31 +33,34 @@ class GUI(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
+        self.to_do_list = ToDoList(tasks=[])
+        
         self.setWindowTitle('TO DO LIST')
         self.setGeometry(680, 300, 400, 500)
         
         #Here are three boxes to get task name, description and priority from user.
         #name
-        name_box = QLineEdit()
-        name_box.setPlaceholderText('Task name')
-        name_box.setClearButtonEnabled(True)
-        name_box.setFixedSize(390, 40)
+        self.name_box = QLineEdit()
+        self.name_box.setPlaceholderText('Task name')
+        self.name_box.setClearButtonEnabled(True)
+        self.name_box.setFixedSize(390, 40)
         #dicreption
-        description_box = QLineEdit()
-        description_box.setPlaceholderText('Task discription')
-        description_box.setClearButtonEnabled(True)
-        description_box.setFixedSize(390, 200)
+        self.description_box = QLineEdit()
+        self.description_box.setPlaceholderText('Task discription')
+        self.description_box.setClearButtonEnabled(True)
+        self.description_box.setFixedSize(390, 200)
         #priority
-        priority_box = QLineEdit()
-        priority_box.setPlaceholderText('Task priority')
-        priority_box.setFixedSize(390, 40)
-        priority_box.setClearButtonEnabled(True)
+        self.priority_box = QLineEdit()
+        self.priority_box.setPlaceholderText('Task priority')
+        self.priority_box.setFixedSize(390, 40)
+        self.priority_box.setClearButtonEnabled(True)
         
         #Here are three buttons to save, delete, save as csv.
         #save
         save_button = QPushButton()
         save_button.setText('Save')
         save_button.setFixedSize(127, 35)
+        save_button.clicked.connect(self.save)
         #delete
         delete_button = QPushButton()
         delete_button.setText('delete')
@@ -72,9 +75,9 @@ class GUI(QWidget):
         layout_v = QVBoxLayout()
         layout_v.setSpacing(5)
         layout_v.setContentsMargins(5, 0, 0, 0)
-        layout_v.addWidget(name_box)
-        layout_v.addWidget(priority_box)
-        layout_v.addWidget(description_box)
+        layout_v.addWidget(self.name_box)
+        layout_v.addWidget(self.priority_box)
+        layout_v.addWidget(self.description_box)
         #horizontal
         layout_h = QHBoxLayout()
         layout_h.setSpacing(2)
@@ -88,6 +91,24 @@ class GUI(QWidget):
         self.setLayout(layout_v)
         
         self.show()
+        
+    def save(self):
+        try:
+            name = self.name_box.text().strip()
+            description = self.description_box.text().strip()
+            priority = int(self.priority_box.text().strip())
+            
+            if name and priority:
+                t = Task(name= name, description= description, priority= priority)
+                self.to_do_list.tasks.append(t)
+                self.to_do_list.show()
+                
+                self.name_box.clear()
+                self.description_box.clear()
+                self.priority_box.clear()
+        except:
+            print("Error: Invalid Input! Fill all the boxes and priority must be a valid integer.")
+                
         
 if __name__ == '__main__':
     app = QApplication(sys.argv)
